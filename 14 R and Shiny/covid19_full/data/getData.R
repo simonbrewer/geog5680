56edee2820351392b00b468add5d4476a5c06481
Simon Brewer <brewersi@gmail.com>
2025-04-30 09:06:50 -0600

    Clean up for pages

--- 14 R and Shiny/covid19_full/data/getData.R
+++
@@ -1,4 +0,0 @@
-library(COVID19)
-
-covid_dat = covid19()
-write.csv(covid_dat, "covid_dat.csv", row.names = FALSE)
--- 16 Projects/Project 1/test.R
+++
@@ -1,7 +0,0 @@
-x = 2 + 2
-print(x)
-
-## Here's a complex equation :)
-
-x1 = 100 * 2.5
-
